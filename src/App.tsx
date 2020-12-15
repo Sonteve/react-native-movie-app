e7c66d15263db8b9fa63a7439033ab1a6025ed02
sonteve <sds11609@gmail.com>
2020-12-15 23:30:59 +0900

    Feat: add splash screen

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,14 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Text} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <>
       <Text>test</Text>
